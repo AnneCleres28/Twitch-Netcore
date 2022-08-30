ba8c50d23545c3e45ea2f3b5f7c0a7bb334bc3b8
AnneCleres28 <annecleres.ac@gmail.com>
2022-08-30 19:13:17 -0300

    Projeto Pronto

---
+++ WepApi/Controllers/v1/GeralController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WepApi.Model;
+
+namespace WepApi.Controllers.v1
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class GeralController : ControllerBase
+    {
+        [HttpGet]
+        [Route("helloworld")]
+        public ActionResult GetHelloWorld()
+        {
+            return Ok("Hello World");
+        }
+
+        [HttpGet]
+        [Route("meunome/{nome}")]
+        public ActionResult GetMeuNome(string nome)
+        {
+            return Ok("Meu nome é " + nome);
+        }
+
+        [HttpGet]
+        [Route("usuario/{nome}/{sobrenome}/{idade}/{endereco}")]
+        public ActionResult GetUsuario(string nome, string sobrenome, int idade, string endereco)
+        {
+            return Ok($@"Nome {nome} Sobrenome {sobrenome} Idade {idade} Endereço {endereco}");
+        }
+
+        [HttpPost]
+        [Route("usuarioobjeto/{usuario}")]
+        public ActionResult GetUsuario([FromBody]Usuario usuario)
+        {
+            return Ok($@"Nome {usuario.Nome} Sobrenome {usuario.Sobrenome} Idade {usuario.Idade} Endereço {usuario.Endereco}");
+        }
+    }
+}
---
+++ WepApi/Controllers/v1/UsuarioController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WepApi.Model;
+
+namespace WepApi.Controllers.v1
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        private static List<Usuario> _usuariosDB;
+
+        public UsuarioController()
+        {
+            if(_usuariosDB == null)
+            _usuariosDB = new List<Usuario>();
+        }
+
+        [HttpGet]
+        public ActionResult GetUsuarios()
+        {
+            return Ok(_usuariosDB);
+        }
+
+        [HttpPost]
+        public ActionResult PostUsuario([FromBody] Usuario usuario)
+        {
+            _usuariosDB.Add(usuario);
+            return Ok();
+        }
+
+        [HttpPut]
+        public ActionResult PutUsuario([FromBody] Usuario usuario)
+        {
+           var usuarioLocalizado = _usuariosDB.Find(a => a.Nome == usuario.Nome);
+
+            if(usuarioLocalizado != null)
+            {
+                usuarioLocalizado.Nome = usuario.Nome;
+                usuarioLocalizado.Sobrenome = usuario.Sobrenome;
+                usuarioLocalizado.Idade = usuario.Idade;
+                usuarioLocalizado.Endereco = usuario.Endereco;
+            } else
+            {
+                return NotFound();
+            }
+
+            return Ok();
+        }
+
+        [HttpDelete]
+        public ActionResult DeleteUsuario ([FromBody] string nome)
+        {
+            var usuarioLocalizado = _usuariosDB.Find(a => a.Nome.Equals(nome));
+
+            if(usuarioLocalizado != null)
+            {
+                _usuariosDB.Remove(usuarioLocalizado);
+            }
+            else
+            {
+                return NotFound();
+            }
+
+            return Ok();
+        }
+    }
+}
